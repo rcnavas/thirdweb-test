a5ffcf0fe550319f132362dad7788a67a3aed249
Roberto Carlos Navas <rcnavas@icloud.com>
2025-06-12 12:07:48 -0600

    Test sponsoraGas

--- src/app/pay/page.tsx
+++ src/app/pay/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { getDefaultToken, PayEmbed } from "thirdweb/react";
-import { defineChain } from "thirdweb";
-import { client } from "@/app/client";
 import { useState } from "react";
+import { getDefaultToken, PayEmbed } from "thirdweb/react";
+import { defineChain, NATIVE_TOKEN_ADDRESS } from "thirdweb";
 import { avalanche, ethereum, polygon, base, bsc } from "thirdweb/chains";
+import {
+  smartWallet,
+  DEFAULT_ACCOUNT_FACTORY_V0_7,
+} from "thirdweb/wallets/smart";
+import { client } from "@/app/client";
 
 const ronin = defineChain(2020);
 
@@ -16,11 +20,24 @@ export default function Page() {
         <PayEmbed
           client={client}
           connectOptions={{
-            chain: avalanche,
-            chains: [avalanche, ethereum, polygon, base, bsc, ronin],
-            
+            chain: polygon,
+            accountAbstraction: {
+              chain: polygon,
+              sponsorGas: true,
+              factoryAddress:  DEFAULT_ACCOUNT_FACTORY_V0_7
+            },
           }}
           locale="es_ES"
+          supportedTokens={{
+            137: [
+              {
+                address: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+                name: "USDC on Polygon",
+                symbol: "USDC",
+                icon: "https://static.cx.metamask.io/api/v1/tokenIcons/137/0x3c499c542cef5e3811e1192ce70d8cc03d5c3359.png",
+              },
+            ],
+          }}
           payOptions={{
             showThirdwebBranding: false,
             metadata: {
@@ -30,9 +47,11 @@ export default function Page() {
             },
             mode: "direct_payment",
             paymentInfo: {
-              chain: avalanche,
+              chain: polygon,
               sellerAddress: "0xd4e8e0b74770880F42cEA9D41fB15899E9F4A45D",
-              amount: "0.0001"
+              amount: "0.01",
+              feePayer: "receiver",
+              token: getDefaultToken(polygon, "USDC"),
             },
             onPurchaseSuccess: (info) => setPurchaseStatus(info),
             purchaseData: {
