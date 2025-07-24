9db5cf736310dc4ef50659c823226ff6331a0e03
Roberto Carlos Navas <rcnavas@icloud.com>
2025-07-23 18:55:16 -0600

    added Spline 3D test page

---
+++ src/app/machine/page.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Page() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/Ta0wTlrqf-rlGl9Q/scene.splinecode" 
+      />
+    </main>
+  );
+}
