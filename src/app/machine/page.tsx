1a36e78c7c6c9e37281d2157f0ad4cb6032e8541
Roberto Carlos Navas <rcnavas@icloud.com>
2025-07-23 19:01:25 -0600

    Setting Variables on Spline Scene

--- src/app/machine/page.tsx
+++ src/app/machine/page.tsx
@@ -1,10 +1,15 @@
-import Spline from '@splinetool/react-spline/next';
+import Spline from "@splinetool/react-spline/next";
+import { Application } from "@splinetool/runtime";
 
 export default function Page() {
+  function onLoad(spline: Application) {
+    spline.setVariables({ X: 0, Z: 0 });
+  }
   return (
     <main>
       <Spline
-        scene="https://prod.spline.design/Ta0wTlrqf-rlGl9Q/scene.splinecode" 
+        scene="https://prod.spline.design/Ta0wTlrqf-rlGl9Q/scene.splinecode"
+        onLoad={onLoad}
       />
     </main>
   );
