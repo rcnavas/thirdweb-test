f896daefe558c02c0d6b4bed9877227f1ea33443
Roberto Carlos Navas <rcnavas@icloud.com>
2025-08-16 17:01:36 -0600

    new Spline test

--- src/app/machine/page.tsx
+++ src/app/machine/page.tsx
@@ -7,9 +7,9 @@ export default function Page() {
     spline.setVariables({ X: 0, Z: 0 });
   }
   return (
-    <div className="h-dvh w-dvw fixed top-0 left-0 m-0 p-0">
+    <div className="h-dvh w-dvw fixed top-0 left-0 m-0 p-0 bg-gradient-to-b from-[#D920F4] to-[#300862]">
       <Spline
-        scene="https://prod.spline.design/Ta0wTlrqf-rlGl9Q/scene.splinecode"
+        scene="https://prod.spline.design/2tiYCWiMhmKzkiSa/scene.splinecode"
         onLoad={onLoad}
       />
     </div>
