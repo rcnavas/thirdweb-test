15970084ae71acd398a4ecfd7711d99b3d440979
Roberto Carlos Navas <rcnavas@icloud.com>
2025-08-22 17:40:56 -0600

    testing Spline integration

--- src/app/machine/page.tsx
+++ src/app/machine/page.tsx
@@ -5,15 +5,15 @@ import { Application } from "@splinetool/runtime";
 
 export default function Page() {
   function onLoad(spline: Application) {
-    spline.setVariables({ X: 0, Z: 0 });
+    spline.setVariables({ X: 0, Z: 0, Counter: 5 });
   }
   function onSplineMouseDown(e: SplineEvent) {
-    if (e.target.name === 'Gift Box') {
-      console.log('Gift Box has been clicked!', e);
+    if (e.target.name === "Gift Box") {
+      console.log("Gift Box has been clicked!", e);
     }
   }
   function onSplineStart(e: SplineEvent) {
-      console.log('Spline Scene Started!', e);
+    console.log("Spline Scene Started!", e);
   }
   return (
     <div className="h-dvh w-dvw fixed top-0 left-0 m-0 p-0 bg-gradient-to-b from-[#D920F4] to-[#300862]">
